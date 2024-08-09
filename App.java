8d559be58b99d36a8fc1623e8e7d58688b4922d4
Shaurya144 <chundawatshaurya@gmail.com>
2024-08-09 13:18:20 +0100

    Add files via upload

--- App.java
+++ App.java
@@ -1,23 +1,20 @@
 import javax.swing.*;
 
 public class App {
+    public static void main(String[] args) throws Exception {
+        int BoardWidth = 360;
+        int BoardHeight = 640;
 
-	public static void main(String[] args) {
-
-		int BoardWidth = 360;
-		int BoardHeight = 640;
-		
-		JFrame frame = new JFrame("");
-
+        JFrame frame = new JFrame("Flappy Bird");
 		frame.setSize(BoardWidth, BoardHeight);
-		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		FlappyBird flappybird = new FlappyBird();
-		frame.add(flappybird);
-		frame.pack();
-		frame.setVisible(true);
-	}
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
 }
